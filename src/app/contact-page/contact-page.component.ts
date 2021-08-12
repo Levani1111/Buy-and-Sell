c215c198a38090dabfa30e0b72e578153f3f83ca
Your Name <you@example.com>
2021-08-12 12:37:46 -0400

    connect the listings page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Listing } from '../types';
+import { ListingsService } from '../listings.service';
 import { fakeListings } from '../fake-data';
 
 @Component({
@@ -11,17 +12,21 @@ import { fakeListings } from '../fake-data';
 export class ContactPageComponent implements OnInit {
   email: string = '';
   message: string = '';
-  listing?: Listing;
+  listing: Listing;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private listingsService: ListingsService,
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find(listing => listing.id === id);
-    this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}!`;
+    this.listingsService.getListingById(id)
+      .subscribe(listing => {
+        this.listing = listing;
+        this.message = `Hi, I'm interested in your ${this.listing.name.toLowerCase()}!`;
+      });
   }
 
   sendMessage(): void {
